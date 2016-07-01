0af608542be82367a9dbb5e3a74cb2c747d990c6
medmex99 <markus@mayya.at>
2016-07-01 09:16:13 +0200

    Initial commit

---
+++ app/src/main/java/at/ms/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ms;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
