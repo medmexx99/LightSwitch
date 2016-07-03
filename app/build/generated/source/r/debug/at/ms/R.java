18f313b19b7331691fddc29ba42b63e315124d65
medmexx99 <markus@mayya.at>
2016-07-03 15:48:07 +0200

    init android studio project

--- app/build/generated/source/r/debug/at/ms/R.java
+++ app/build/generated/source/r/debug/at/ms/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int roundedborder=0x7f020001;
     }
     public static final class id {
         public static final int btCoLampe1=0x7f060003;
@@ -19,6 +20,8 @@ public final class R {
         public static final int btEtLampe1=0x7f060001;
         public static final int btEtLampe2=0x7f060002;
         public static final int esstischTextView=0x7f060000;
+        public static final int receiveTextView=0x7f060006;
+        public static final int sendTextView=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
